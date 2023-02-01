72e3979d9bc115c77b0042777c1375a68a3dd2ea
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-01-31 20:27:11 -0800

    add schema.sql to specify database schema

---
+++ db/schema.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS hris_db;
+CREATE DATABASE hris_db;
+
+USE hris_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(80) NOT NULL,
+    department_id INT NOT NULL,
+    salary INT NOT NULL,
+    CHECK (salary <= 2000000),
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);
+
+ALTER TABLE roles AUTO_INCREMENT=200;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    role_id INT NOT NULL,
+    department_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+);
+
+ALTER TABLE employees AUTO_INCREMENT=1000;
+
+SHOW COLUMNS FROM departments;
+SHOW COLUMNS FROM roles;
+SHOW COLUMNS FROM employees;
